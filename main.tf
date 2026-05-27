785f583c19d7c34267fbb65a7238578bcc36d112
JohnnyBungo <john.guentensperger@gmail.com>
2026-05-27 13:32:43 +0200

    feat: terraform state

--- main.tf
+++ main.tf
@@ -17,3 +17,18 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_storage_bucket" "terraform_backend" {
+  name          = "${var.project_id}-tfbackend"
+  location      = "EUROPE-WEST6"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "project-3b05128c-8500-4a22-b1c-tfbackend"
+    prefix = "terraform/state"
+  }
+}
+ 
